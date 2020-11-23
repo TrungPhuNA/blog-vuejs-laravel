b1008ad54fab6438dd273683f6c71d43eef5c416
Phan Trung Phú <phupt@dol-tech.com>
2020-11-23 22:56:11 +0700

    add icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Học Lập Trình Online Miễn Phí</title>
+        <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="all">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
     </head>
     <body>
         <div id="app">
